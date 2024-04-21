4e3553eb20c5fd381d718c49db519d1641980ef9
Aviwe <aviwenkambule@gmail.com>
2024-04-21 15:08:57 -0700

    0x0A-configuration_management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 # creates a file
 file {  '/tmp/school':
-	mode    => '0744',
-	owner   => 'www-data',
-	group   => 'www-data',
-	content => 'I love puppet'
+ ensure  => file,
+ mode    => '0744',
+ owner   => 'www-data',
+ group   => 'www-data',
+ content => 'I love Puppet',
 }
